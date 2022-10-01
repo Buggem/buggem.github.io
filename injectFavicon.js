dc94e49a39aab800ae4ae51507235830a77449ce
Buggem <66670928+Buggem@users.noreply.github.com>
2022-10-01 10:26:57 +1000

    Create injectFavicon.js

---
+++ injectFavicon.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+  var favicon = document.createElement("link");
+  favicon.rel = "icon";
+  favicon.href = "https:\/\/deltarune.com\/assets\/images\/coolgifs\/spamdance.gif";
+  document.head.appendChild(favicon);
+});
