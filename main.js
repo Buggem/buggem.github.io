773f9438dd6b922045de200dd9906c5f45d7ad45
Buggem <66670928+Buggem@users.noreply.github.com>
2022-10-06 18:47:57 +1100

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function() {
     for(link of hiddenLinks) {
       link.addEventListener("click", function() {
         console.log(this.getAttribute('tvhl'));
+        window.location.href = this.getAttribute('tvhl');
       });
     }
   };
